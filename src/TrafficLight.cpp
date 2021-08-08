c046c47d176352234b3e4792daba7497aa6e7c5c
Ruben Kentchim <rubstarjohn@yahoo.fr>
2021-08-08 16:41:51 +0200

    implementation of FP.4a and FP.4.b

--- src/TrafficLight.cpp
+++ src/TrafficLight.cpp
@@ -15,14 +15,26 @@ T MessageQueue<T>::receive()
     // to wait for and receive new messages and pull them from the queue using move semantics. 
     // The received object should then be returned by the receive function. 
 }
+*/
 
 template <typename T>
 void MessageQueue<T>::send(T &&msg)
 {
     // FP.4a : The method send should use the mechanisms std::lock_guard<std::mutex> 
     // as well as _condition.notify_one() to add a new message to the queue and afterwards send a notification.
+
+    // simulate some work
+    std::this_thread::sleep_for(std::chrono::milliseconds(100));
+
+    // perform vector modification under the lock
+    std::lock_guard<std::mutex> uLock(_mutex);
+
+    // add vector to queue
+    std::cout << "   Message " << msg << " has been sent to the queue" << std::endl;
+    _messages.push_back(std::move(msg));
+    _cond.notify_one(); // notify client after pushing new Vehicle into vector
 }
-*/
+
 
 /* Implementation of class "TrafficLight" */
 
@@ -99,8 +111,11 @@ void TrafficLight::cycleThroughPhases()
             }
 
             cycle_duration = rand() % (high - low + 1) + low;
-            startTime = std::chrono::system_clock::now();
             
+            startTime = std::chrono::system_clock::now();
+
+            // sends an update method to the message queue
+            _msgQueue.send(std::move(_currentPhase));
         }
     }
 
